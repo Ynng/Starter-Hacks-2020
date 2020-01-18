d5bc378788b366437aed74d29d1922682ba3ee71
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-18 17:06:05 -0500

    hmmm Co-authored-by: CheatCod <2892084432j@gmail.com> Co-authored-by: Owen <owenhu0410@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Starter-Hacks-2020" 
+# Starter-Hacks-2020
+
+## Acknowledgement
+
+[js-calc](https://paiv.github.io/blog/2016/03/23/js-calc)
---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * List of keywords
+ * Assignment: "is"
+ * And: "and"
+ * Addition: "plus", "add", "+"
+ * Subtraction: "minus","subtract","-"
+ * Multiplication: "times", "multiply", "*"
+ * Division: "divide", "/" 
+ * */
+
+/**
+ * Topic selection: "topic"
+ * Grab variables used in the topic and convert them to keywords
+ * 
+ * Topic is E&M
+ * Define electric_potential as V
+] * 
+ */
+
+
+
+function StringInput(input) {
+
+}
+
+function is(i)
