3e5b03ffc79a417a553a6da249f25c14cf3fe1de
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-19 03:54:35 -0500

    Enormous changes on auto recommandation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var input;
 var output;
 // var varList = new Map();
 
+var ctrl_key_down = false;
 function findWord(word, str) {
     return RegExp('\\b' + word + '\\b').test(str)
 }
@@ -24,14 +25,20 @@ function parseInput() {
 
 var focused;
 
-$(document).on('keydown, keyup, mousedown, mouseup', function() {
-    focused = $(':focus');
+function updateFocused() {
+    if ($(':focus').hasClass("mainInput")) {
+        focused = $(':focus');
+    }
+}
+
+$(document).on('keydown, keyup, mousedown, mouseup', function () {
+    updateFocused();
     $('#box').css({ 'left': (letter_size * focused[0].selectionStart) + 'px', 'top': $(focused).offset().top + $(focused).height() });
 })
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".mainInput")[0].focus();
-    focused = $(':focus');
+    updateFocused();
 })
 
 function addNewLine() {
@@ -54,42 +61,83 @@ function removeLine() {
 // $(".mainInput").on('input', function () {
 //   parseInput();
 // });
-$(".mainInputContainer").on('keydown', function(e) {
-    if (e.which == 8 && focused.val().length == 0) {
-        e.preventDefault();
-        removeLine();
-        parseInput();
+$(".mainInputContainer").on('keydown', function (e) {
+    if(e.which == 17){
+        ctrl_key_down = true;
     }
-    if (e.which == 40) {
-        if ($(focused).next().length <= 0) return;
-        e.preventDefault();
-        $(focused).next().attr("id", "ToBeFocused");
-        console.log(focused[0].selectionStart);
-        $("#ToBeFocused").focus();
-        $("#ToBeFocused")[0].setSelectionRange(focused[0].selectionStart, focused[0].selectionStart);
-        $("#ToBeFocused").attr("id", "");
+    if($("#autocomplete-list").children().length>0){
+        var x = document.getElementById("autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        console.log(x)
+        if (e.keyCode == 40) {
+            e.preventDefault();
+    
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            e.preventDefault();
+    
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        } else {
+            autocomplete_input_change(countries, e.keyCode);
+        }
+    }else{
+        autocomplete_input_change(countries, e.keyCode);
+        if (e.which == 8 && focused.val().length == 0) {
+            e.preventDefault();
+            removeLine();
+            parseInput();
+        }
+        if (e.which == 40) {
+            if ($(focused).next().length <= 0) return;
+            e.preventDefault();
+            $(focused).next().attr("id", "ToBeFocused");
+            console.log(focused[0].selectionStart);
+            $("#ToBeFocused").focus();
+            $("#ToBeFocused")[0].setSelectionRange(focused[0].selectionStart, focused[0].selectionStart);
+            $("#ToBeFocused").attr("id", "");
+            autocomplete = true;
+        }
+        if (e.which == 38) {
+            if ($(focused).prev().length <= 0) return;
+            e.preventDefault();
+            $(focused).prev().attr("id", "ToBeFocused");
+            console.log(focused[0].selectionStart);
+            $("#ToBeFocused").focus();
+            $("#ToBeFocused")[0].setSelectionRange(focused[0].selectionStart, focused[0].selectionStart);
+            $("#ToBeFocused").attr("id", "");
+            autocomplete = true;
+        }
+        if (e.which == 13) {
+            //enter key
+            addNewLine();
+        }
     }
-    if (e.which == 38) {
-        if ($(focused).prev().length <= 0) return;
-        e.preventDefault();
-        $(focused).prev().attr("id", "ToBeFocused");
-        console.log(focused[0].selectionStart);
-        $("#ToBeFocused").focus();
-        $("#ToBeFocused")[0].setSelectionRange(focused[0].selectionStart, focused[0].selectionStart);
-        $("#ToBeFocused").attr("id", "");
-    }
+    
 })
 
-$(".mainInputContainer").on('keyup', function(e) {
-    // console.log(e.which)
-    parseInput();
-    if (e.which == 13) {
-        //enter key
-        addNewLine();
+$(".mainInputContainer").on('keyup', function (e) {
+    if(e.which == 17){
+        ctrl_key_down = false;
     }
-    focused = $(':focus');
+    parseInput();
     $('#box').css({ 'left': (letter_size * focused[0].selectionStart) + 'px', 'top': $(focused).offset().top + $(focused).height() });
-    autocomplete(focused[0], countries);
+    // console.log(e.which)
+    updateFocused();
 });
 
 var scope = {};
@@ -118,7 +166,7 @@ function StringInput(line, lineNumber) {
         //TODO: pass shit from jsonparser to here
         solveAlgebra();
     }
-    
+
     try {
         return math.eval(line, scope);
     } catch (e) {
@@ -139,4 +187,80 @@ function StringInput(line, lineNumber) {
 
 
 /*An array containing all the country names in the world:*/
-var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
+
+var currentFocus;
+var autocomplete = false;
+function autocomplete_input_change(arr, keycode) {
+    if(ctrl_key_down){
+        var a, b, i, val = focused.val();
+    }else{
+        var a, b, i, val = focused.val() + String.fromCharCode(keycode);
+    }
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) { return false; }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    document.getElementById("box").appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function (e) {
+                /*insert the value for the autocomplete text field:*/
+                $(focused)[0].value = this.children[1].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+        }
+    }
+    autocomplete = true;
+}
+
+function addActive(x) {
+    console.log(x);
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+}
+function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+    }
+}
+function closeAllLists(elmnt) {
+    // console.log(focused[0])
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i]) {
+            x[i].parentNode.removeChild(x[i]);
+        }
+    }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
