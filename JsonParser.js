9ba014695e4a4e566b70bfc5af3d1e2b1582f0c4
CheatCod <2892084432j@gmail.com>
2020-01-18 23:32:59 -0500

    jsonparser now return name[]

--- JsonParser.js
+++ JsonParser.js
@@ -5,16 +5,21 @@ class PhysicQuestions {
             this.Variables = Variables.slice();
         }
         this.Formula = Formula;
-      }
-      
+    }
+
 }
 function writeObjToJson() {
     variables = ["time", "distance"];
     variables2 = ["time2", "distance2"];
     let physicsQuestion = new PhysicQuestions('Velocity', variables, 'distance/time');
     let physicsQuestion2 = new PhysicQuestions('Acceleration', variables, 'distance/time2');
-    var arr = [ physicsQuestion,  physicsQuestion2];
-    console.log(JSON.stringify(arr));
+    var arr = [physicsQuestion, physicsQuestion2];
+    names = [];
+    for (i = 0; i < arr.length; i++) {
+        names[i] = arr[i]["Name"];
+    }
+    console.log(name);
+    return name;
 
 }
 
