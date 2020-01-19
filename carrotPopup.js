3d51c1849444ecc046ff7ad42b5efb55bc972974
3Nya3 <sunnyzedragon@gmail.com>
2020-01-19 02:14:30 -0500

    delete obsolete file

--- carrotPopup.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var letter_size = 6;
-    $("input.mainInput").keyup(function(e) {
-        move_size = $(this).val().length * letter_size;
-        console.log(move_size);
-        $('#box').css({'left': move_size+'px'});
-    });
-});
