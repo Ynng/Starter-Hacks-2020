b38a7258e47a4fd52c35d03a7aa20273bc6e46be
Edge7481 <owenhu0410@gmail.com>
2020-01-19 00:33:07 -0500

    root solver

--- algebra.js
+++ algebra.js
@@ -1,17 +1,21 @@
 var Algebrite = require('algebrite');
 
-var input = "0 = -1 * B + u0 * I / 2 / pi / d";
+var input = "-1 * B + u0 * I / 2 / pi / d";
 
-var knowns = { "B": 1, "u0": 0.00004, "pi": 3.14, "d": 0.5 };
-console.log(knowns);
-
+var knowns = new Map();
+knowns.set("B", 5.1);
+knowns.set("u0", 4.11);
+knowns.set("pi", 3.14);
+knowns.set("d", 5);
 var splitString = input.split(" ");
 var substitutedStr = "";
 for (var i = 0; i < splitString.length; i++) {
-    if (knowns.has(splitString.at[i])) {
-        splitString.at[i] = knowns.get(splitString.at[i]);
+    if (knowns.has(splitString[i])) {
+        splitString[i] = knowns.get(splitString[i]);
     }
     substitutedStr += splitString[i];
+    console.log(substitutedStr);
+
 }
 
-console.log(Algebrite.roots(substitutedStr));
+console.log(Algebrite.nroots(substitutedStr).d);
