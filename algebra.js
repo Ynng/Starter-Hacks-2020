b760a68497217aae89907ca7e21f33e02d74a238
Edge7481 <owenhu0410@gmail.com>
2020-01-19 00:12:09 -0500

    change json

--- algebra.js
+++ algebra.js
@@ -3,12 +3,15 @@ var Algebrite = require('algebrite');
 var input = "0 = -1 * B + u0 * I / 2 / pi / d";
 
 var knowns = { "B": 1, "u0": 0.00004, "pi": 3.14, "d": 0.5 };
-var substitutedStr;
+console.log(knowns);
 
-var splitString = knowns.split(" ");
-for (var i = 0; i < input.length; i++) {
-
+var splitString = input.split(" ");
+var substitutedStr = "";
+for (var i = 0; i < splitString.length; i++) {
+    if (knowns.has(splitString.at[i])) {
+        splitString.at[i] = knowns.get(splitString.at[i]);
+    }
+    substitutedStr += splitString[i];
 }
 
 console.log(Algebrite.roots(substitutedStr));
-if ("00")
