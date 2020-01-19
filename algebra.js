1a3b4d453861976336f4ab8aacd020aa3a99f9b1
Edge7481 <owenhu0410@gmail.com>
2020-01-19 01:27:08 -0500

    retarded library is now working

--- algebra.js
+++ algebra.js
@@ -1,6 +1,28 @@
+function gcd(a, b) {
+    return (b) ? gcd(b, a % b) : a;
+}
+
+function convert(_decimal) {
+    console.log(_decimal);
+    if (_decimal == parseInt(_decimal)) {
+        return _decimal;
+    } else {
+        var top = _decimal.toString().includes(".") ? _decimal.toString().replace(/\d+[.]/, '') : 0;
+        var bottom = Math.pow(10, top.toString().replace('-', '').length);
+        if (_decimal >= 1) {
+            top = +top + (Math.floor(_decimal) * bottom);
+        } else if (_decimal <= -1) {
+            top = +top + (Math.ceil(_decimal) * bottom);
+        }
+
+        var x = Math.abs(gcd(top, bottom));
+        return top + "/" + bottom;
+    }
+};
+
 var Algebrite = require('algebrite');
 
-var input = "-1 * B + u0 * I * x / 2 / pi / d";
+var input = "-1 * B + u0 * I / 2 / pi / d";
 
 var key = "I"; //UNKMNOWN VARIABLE
 input = input.replace(key, "x");
@@ -13,13 +35,14 @@ var splitString = input.split(" ");
 var substitutedStr = "";
 for (var i = 0; i < splitString.length; i++) {
     if (knowns.has(splitString[i])) {
-        splitString[i] = knowns.get(splitString[i]);
+        console.log(knowns.get(splitString[i]));
+        splitString[i] = convert(knowns.get(splitString[i]));
     }
     substitutedStr += splitString[i];
     console.log(substitutedStr);
 
 }
-var sol = Algebritroots("x*x/2");
+var sol = Algebrite.nroots(substitutedStr);
 console.log(sol);
 if (sol.tensor == null) {
     console.log(sol.d);
--- bundle.js
+++ bundle.js
@@ -1,7 +1,29 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+function gcd(a, b) {
+    return (b) ? gcd(b, a % b) : a;
+}
+
+function convert(_decimal) {
+    console.log(_decimal);
+    if (_decimal == parseInt(_decimal)) {
+        return _decimal;
+    } else {
+        var top = _decimal.toString().includes(".") ? _decimal.toString().replace(/\d+[.]/, '') : 0;
+        var bottom = Math.pow(10, top.toString().replace('-', '').length);
+        if (_decimal >= 1) {
+            top = +top + (Math.floor(_decimal) * bottom);
+        } else if (_decimal <= -1) {
+            top = +top + (Math.ceil(_decimal) * bottom);
+        }
+
+        var x = Math.abs(gcd(top, bottom));
+        return top + "/" + bottom;
+    }
+};
+
 var Algebrite = require('algebrite');
 
-var input = "-1 * B + u0 * I * x / 2 / pi / d";
+var input = "-1 * B + u0 * I / 2 / pi / d";
 
 var key = "I"; //UNKMNOWN VARIABLE
 input = input.replace(key, "x");
@@ -14,13 +36,14 @@ var splitString = input.split(" ");
 var substitutedStr = "";
 for (var i = 0; i < splitString.length; i++) {
     if (knowns.has(splitString[i])) {
-        splitString[i] = knowns.get(splitString[i]);
+        console.log(knowns.get(splitString[i]));
+        splitString[i] = convert(knowns.get(splitString[i]));
     }
     substitutedStr += splitString[i];
     console.log(substitutedStr);
 
 }
-var sol = Algebrite.nroots("x*x/2");
+var sol = Algebrite.nroots(substitutedStr);
 console.log(sol);
 if (sol.tensor == null) {
     console.log(sol.d);
